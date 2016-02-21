0371dc5d9d908089b85bfe33345131eddad3dd78
Soheb <soheby@gmail.com>
2016-02-21 18:55:06 +0000

    Removing depot tools because it doesn't set it up properly, changing chromium dir to crosswalk

--- build_crosswalk_soheb.sh
+++ build_crosswalk_soheb.sh
@@ -4,15 +4,8 @@ set -e
 
 export XWALK_OS_ANDROID=1
 
-cd ~
-
-if [ ! -d "depot_tools" ]; then
-	git clone https://chromium.googlesource.com/chromium/tools/depot_tools.git
-	export PATH=`pwd`/depot_tools:"$PATH"
-fi
-
-mkdir ~/chromium
-cd ~/chromium
+mkdir ~/crosswalk
+cd ~/crosswalk
 
 gclient config --name=src/xwalk https://github.com/somoso/crosswalk.git@origin/crosswalk-16
 
@@ -23,7 +16,7 @@ gclient sync
 echo "{ 'GYP_DEFINES': 'OS=android target_arch=arm', }" > chromium.gyp_env
 
 export GYP_GENERATORS='ninja'
-cd ~/chromium/src
+cd ~/crosswalk/src
 python xwalk/gyp_xwalk
 
 ninja -C out/Release xwalk_core_library_aar xwalk_shared_library_aar
